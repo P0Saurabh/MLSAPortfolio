727ad5067006670f1ccececf83e6217a7b4c334b
P0Saurabh <111894219+P0Saurabh@users.noreply.github.com>
2024-05-05 07:47:41 +0000

    info change

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "SAaurabh Pawar",
+  title: "Web & AI/ML developer",
+  email: "saurabhsp0402.com",
+  gitHub: "https://github.com/P0Saurabh",
+  instagram: "https://www.instagram.com/saurabh_sp04/",
+  linkedIn: "https://www.linkedin.com/in/saurabh-pawar-699901229/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
