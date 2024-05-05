3ccc8a63b034041348a3973146c3bbd0fe213fe9
P0Saurabh <111894219+P0Saurabh@users.noreply.github.com>
2024-05-05 15:29:02 +0000

    video

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "Saurabh Pawar here, diving into the world of Web & AI/ML development. As a student in AI/ML, I'm all about exploring new tech and crafting innovative solutions. Complex challenges? Bring 'em on. I'm here to make waves in the tech scene!";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -32,11 +32,7 @@ const description =
  */
 const skillsList = [
   "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Ai/ml",
 ];
 
 /**
@@ -45,7 +41,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my AI?ML experience I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
